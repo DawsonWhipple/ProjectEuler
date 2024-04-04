4c2540d4cbca2f2d9cff4eb2876a7e451d6ef51d
DawsonWhippleWSU <dlwhip@outlook.com>
2024-04-04 10:06:26 +0200

    Solved problem 16

--- code/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    num = 10000000000000000;
-    cout << "Largest perfect cube of " << num << " is: " << largestPerfectCube(num);
-    cout << " It takes " << numberOfSteps(num) << " steps to get to 0" <<'\n';
-    cout << "Sum of largest perfect cubes of " << num << " is: " << sumOfPerfectCubes(num) << '\n';
